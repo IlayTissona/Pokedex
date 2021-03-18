7b49c16a822f9cabdc493aac2e74520904f696ca
IlayTissona <ilay211@gmail.com>
2021-03-18 18:52:46 +0200

    backend hopefully done

--- server/routes/collection.js
+++ server/routes/collection.js
@@ -1,5 +1,4 @@
 const { Router } = require("express");
-const fs = require("fs");
 const axios = require("axios");
 const {
   setExisting,
--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -26,21 +26,4 @@ pokemon.get("/:name", async (req, res) => {
   res.json(setExisting(pokemonFullObj));
 });
 
-pokemon.get("/catch/:name", async (req, res) => {
-  const pokemonName = req.params.name;
-  let pokemonFullObj = getExisting(pokemonName);
-  if (!pokemonFullObj) {
-    try {
-      const responseFullObj = await axios.get(
-        collectionURL + "/" + pokemonName
-      );
-      pokemonFullObj = generateResponsePokemon(responseFullObj.data);
-    } catch (e) {
-      return res.send(e.message);
-    }
-  }
-  pokemonFullObj.catched = !pokemonFullObj.catched;
-  res.json(setExisting(pokemonFullObj));
-});
-
 module.exports = pokemon;
