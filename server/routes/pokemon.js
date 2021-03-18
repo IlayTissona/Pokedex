3e3fc96ef371c1f75ace985aee68ef2ef9f927e2
IlayTissona <ilay211@gmail.com>
2021-03-18 15:01:39 +0200

    added pictures and types

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const axios = require("axios");
+const fs = require("fs");
 require("dotenv").config();
 
 const collectionURL = process.env.POKEAPI_URL + "pokemon";
@@ -7,10 +8,34 @@ const pokemon = Router();
 
 pokemon.get("/:name", async (req, res) => {
   const pokemonName = req.params.name;
+  if (
+    fs.existsSync(
+      `${process.cwd()}/routes/JSON-data/recieved/${pokemonName}.json`
+    )
+  )
+    return res.send(
+      fs.readFileSync(
+        `${process.cwd()}/routes/JSON-data/recieved/${pokemonName}.json`
+      )
+    );
+
   const responseFullObj = await axios
     .get(collectionURL + "/" + pokemonName)
     .catch((e) => res.send(e));
   const pokemonFullObj = generateResponsePokemon(responseFullObj.data);
+  if (!pokemonFullObj) {
+    return res.sendStatus(404);
+  }
+  res.json(pokemonFullObj);
+  fs.writeFileSync(
+    `${process.cwd()}/routes/JSON-data/recieved/${pokemonName}.json`,
+    JSON.stringify(pokemonFullObj)
+  );
+});
+
+pokemon.get("/catch/:name", async (req, res) => {
+  const pokemonName = req.params.name;
+
   if (!pokemonFullObj) {
     return res.sendStatus(404);
   }
@@ -21,7 +46,34 @@ pokemon.get("/:name", async (req, res) => {
 
 //----------------------------------------------Functions-------------------------------------------------//
 function generateResponsePokemon(fullObj) {
-  const { name, height, weight, types, id } = fullObj;
-  return { name, height, weight, types, id };
+  const { name, height, weight, id } = fullObj;
+  const types = fullObj.types.map((typeObj) => typeObj.type.name);
+  const { back_default, front_default } = fullObj.sprites;
+  const catched = isCatched(id);
+  return {
+    name,
+    height,
+    weight,
+    types,
+    id,
+    catched,
+    back_default,
+    front_default,
+  };
+}
+
+function isCatched(id) {
+  const catched = JSON.parse(
+    fs.readFileSync(process.cwd() + "/routes/JSON-data/catchedIds.json")
+  );
+  return catched.includes(id);
+}
+
+function catchPokemon(pokemonObj) {
+  const catchedIds = JSON.parse(
+    fs.readFileSync(process.cwd() + "/routes/JSON-data/catchedIds.json")
+  );
+  catchedIds.push(pokemonObj.id);
+  return true;
 }
 module.exports = pokemon;
