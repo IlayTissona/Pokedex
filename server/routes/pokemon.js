19bbb333dc48438f4dcd5489a8ff8bb0037b86e1
IlayTissona <ilay211@gmail.com>
2021-03-18 14:24:12 +0200

    first GET endpoint pokemon route

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -1,9 +1,27 @@
 const { Router } = require("express");
+const axios = require("axios");
+require("dotenv").config();
 
+const collectionURL = process.env.POKEAPI_URL + "pokemon";
 const pokemon = Router();
 
-pokemon.get("/", (req, res) => {
-  res.send("Pokemon route");
+pokemon.get("/:name", async (req, res) => {
+  const pokemonName = req.params.name;
+  const responseFullObj = await axios
+    .get(collectionURL + "/" + pokemonName)
+    .catch((e) => res.send(e));
+  const pokemonFullObj = generateResponsePokemon(responseFullObj.data);
+  if (!pokemonFullObj) {
+    return res.sendStatus(404);
+  }
+  res.send(pokemonFullObj);
 });
 
+//name. height, weight, types, catched, id
+
+//----------------------------------------------Functions-------------------------------------------------//
+function generateResponsePokemon(fullObj) {
+  const { name, height, weight, types, id } = fullObj;
+  return { name, height, weight, types, id };
+}
 module.exports = pokemon;
